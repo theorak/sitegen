9557fc1141824c2693173d4777060398279d8064
theorak <proensch@prdev.eu>
2024-12-13 13:00:58 +0100

    added spliting for links and images including tests

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # imports
 from textnode import TextNode, TextType
 from leafnode import LeafNode
+import re
 
 # main
 def main():
@@ -30,5 +31,81 @@ def text_node_to_html_node(text_node: TextNode):
         case _:
             raise Exception("Unkown TextType for TextNode")
     return html_node
+
+# splits nodes by delimiter into sub nodes of the desired type
+def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: TextType):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT: # passthrough non-text nodes
+            new_nodes.extend[node]
+        else:
+            parts = node.text.split(delimiter)
+            if len(parts) == 3:
+                new_nodes.extend([
+                    TextNode(parts[0], TextType.TEXT),
+                    TextNode(parts[1], text_type),
+                    TextNode(parts[2], TextType.TEXT)
+                ])
+            else:
+                raise Exception(f"Text Node does not properly terminate delimiters, should have two occurances of '{delimiter}'")        
+    return new_nodes
+
+# Markdown parser for images
+def extract_markdown_images(text: str):
+    return  re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+# Markdown parser for links
+def extract_markdown_links(text: str):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def split_nodes_image(old_nodes: list):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT: # passthrough non-text nodes
+            new_nodes.extend[node]
+        else:
+            matches = extract_markdown_images(node.text)  # list of tupels
+            if len(matches) > 0:
+                text = node.text # will move through text
+                for match in matches:
+                    image_alt = match[0]
+                    image_link = match[1]
+                    parts = text.split(f"![{image_alt}]({image_link})", 1) # split current position in text only once into image and rest
+                    if parts[0] != "":
+                        text = parts[1]
+                        new_nodes.extend([
+                            TextNode(parts[0], TextType.TEXT),
+                            TextNode(image_alt, TextType.IMAGE, image_link)
+                        ])
+                    else:
+                        new_nodes.extend([
+                            TextNode(image_alt, TextType.IMAGE, image_link)
+                        ])
+    return new_nodes
+
+def split_nodes_link(old_nodes: list):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT: # passthrough non-text nodes
+            new_nodes.extend[node]
+        else:
+            matches = extract_markdown_links(node.text)  # list of tupels
+            if len(matches) > 0:
+                text = node.text # will move through text
+                for match in matches:
+                    link_text = match[0]
+                    url = match[1]
+                    parts = text.split(f"[{link_text}]({url})", 1) # split current position in text only once into image and rest
+                    if parts[0] != "":
+                        text = parts[1]
+                        new_nodes.extend([
+                            TextNode(parts[0], TextType.TEXT),
+                            TextNode(link_text, TextType.LINK, url)
+                        ])
+                    else:
+                        new_nodes.extend([
+                            TextNode(link_text, TextType.LINK, url)
+                        ])
+    return new_nodes
 
 main()
--- src/test_main.py
+++ src/test_main.py
@@ -8,10 +8,87 @@ class TestTextNode(unittest.TestCase):
         text_node = TextNode("Normal Text", TextType.TEXT)
         self.assertIsInstance(text_node_to_html_node(text_node), LeafNode)
 
-    def test_leaf_node_html(self):
-        print("Main Test: test_leaf_node_html")
+    def test_link_node_to_html_node(self):
+        print("Main Test: test_link_node_to_html_node")
         text_node = TextNode("Link", TextType.LINK, "https://www.google.com")
         self.assertEqual(text_node_to_html_node(text_node).to_html(), '<a href="https://www.google.com">Link</a>')
+
+    def test_split_nodes_delimiter(self):
+        print("Main Test: test_split_nodes_delimiter")
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ]
+        )
+
+    def test_split_nodes_delimiter_error(self):
+        print("Main Test: test_split_nodes_delimiter_error")
+        node = TextNode("Wrong text to *bold", TextType.TEXT)
+        with self.assertRaises(Exception):
+            split_nodes_delimiter([node], "*", TextType.BOLD)
+
+    def test_markdown_parsers(self):
+        print("Main Test: test_markdown_parsers")
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        tupels = extract_markdown_images(text)
+        self.assertEqual(tupels, [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+
+        text2 = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        tupels2 = extract_markdown_links(text2)
+        self.assertEqual(tupels2, [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
+
+    def test_split_nodes_image(self):
+        print("Main Test: test_split_nodes_image")
+        node = TextNode(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("rick roll", TextType.IMAGE, "https://i.imgur.com/aKaOqIh.gif"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("obi wan", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg")
+            ]
+        )
+
+    def test_split_nodes_link(self):
+        print("Main Test: test_split_nodes_link")
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev")
+            ]
+        )
+
+    def test_split_nodes_image_edge(self):
+        print("Main Test: test_split_nodes_image_edge")
+        node = TextNode(
+            "![edge case](https://i.imgur.com/aKaOqIh.gif)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("edge case", TextType.IMAGE, "https://i.imgur.com/aKaOqIh.gif"),
+            ]
+        )
 
 if __name__ == "__main__":
     unittest.main()
