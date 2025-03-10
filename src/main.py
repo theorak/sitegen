efddd2c0ba0998b0ccb6068d23e34f13d8f3068c
theorak <proensch@prdev.eu>
2025-03-10 11:29:50 +0100

    starting file gen and movement

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 # imports
+import shutil
 from textnode import TextNode, TextType
 from nodeparser import Nodeparser
 
 # main
 def main():
-    text_node = TextNode("Starting Main", TextType.BOLD)
-    print(text_node)   
+    copy_recursive('static/', 'public/')
+
+def copy_recursive(origin_dir, target_dir):
+    shutil.rmtree(target_dir)
+    shutil.copy(origin_dir, target_dir)
 
 main()
